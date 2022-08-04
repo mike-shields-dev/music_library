4f8e428c287e5a5fb29bdd7c05303fb74ee18bf8
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-04 12:51:15 +0100

    feature: POST /artist creates a new artist in database using request body

--- tests/artist-create.js
+++ tests/artist-create.js
@@ -1,8 +1,19 @@
 const { expect } = require("chai")
 const request = require("supertest")
+const getDb = require("../src/services/db")
 const app = require("../src/app")
 
 describe("create artist", () => {
+  let db
+  beforeEach(async () => {
+    db = await getDb()
+  })
+
+  afterEach(async () => {
+    await db.query("DELETE FROM Artist")
+    await db.end()
+  })
+
   describe("/artist", () => {
     describe("POST", () => {
       it("creates a new artist in the database", async () => {
@@ -11,6 +22,13 @@ describe("create artist", () => {
           genre: "Test Genre",
         })
         expect(res.status).to.equal(201)
+
+        const [[artistEntries]] = await db.query(
+          "SELECT * FROM Artist WHERE name = 'Test Artist'"
+        )
+
+        expect(artistEntries.name).to.equal("Test Artist")
+        expect(artistEntries.genre).to.equal("Test Genre")
       })
     })
   })
