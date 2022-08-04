5a38c898733e61ba3da5ff465de8db0d6e85f5b3
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-04 13:39:29 +0100

    refactor: POST /artist, sanitise sql params

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -4,12 +4,13 @@ exports.create = async (req, res) => {
   const db = await getDb()
   const { name, genre } = req.body
   try {
-    await db.query(`
-      INSERT INTO Artist (name, genre) 
-      VALUES ('${name}', '${genre}')
-    `)
+    await db.query("INSERT INTO Artist SET ?", {
+      name,
+      genre,
+    })
     res.sendStatus(201)
   } catch (err) {
+    console.log(err)
     res.sendStatus(500).json(err)
   } finally {
     await db.end()
