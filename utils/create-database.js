7ad53798360d5bbc73b07449387e3c922990c7db
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-04 20:57:33 +0100

    fix: correct typo

--- utils/create-database.js
+++ utils/create-database.js
@@ -19,7 +19,7 @@ require("dotenv").config({
 // destructure environment variables from process.env
 const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env
 
-// This asyncronous function will run before app
+// This asynchronous function will run before app
 const setUpDatabase = async () => {
   try {
     // connect to the database
