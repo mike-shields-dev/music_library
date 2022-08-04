49c6afc6df4f748d65ce909b66f49944b6609fe2
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-04 14:13:09 +0100

    feature: POST /artist returns all artists in the database

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,5 +4,6 @@ const artistController = require("../controllers/artist")
 const router = Router()
 
 router.post("/", artistController.create)
+router.get("/", artistController.read)
 
 module.exports = router
