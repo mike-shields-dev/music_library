6514932bdf2dfb95047bfbe878e0deb44f7cc7d5
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-04 20:59:16 +0100

    test: create album

---
+++ tests/album/create.js
@@ -0,0 +1,35 @@
+const { expect } = require("chai")
+const request = require("supertest")
+const getDb = require("../../src/services/db")
+const app = require("../../src/app")
+
+describe("create album", () => {
+  let db
+  beforeEach(async () => {
+    db = await getDb()
+  })
+
+  afterEach(async () => {
+    await db.query("DELETE FROM Album")
+    await db.end()
+  })
+
+  describe("/album", () => {
+    describe("POST", () => {
+      it("creates a new album in the database", async () => {
+        const res = await request(app).post("/album").send({
+          name: "Test Album",
+          artistId: 1,
+        })
+        expect(res.status).to.equal(201)
+
+        const [[albumEntries]] = await db.query(
+          'SELECT * FROM Album WHERE name = "Test Album"'
+        )
+
+        expect(albumEntries.name).to.equal("Test Album")
+        expect(albumEntries.artistId).to.equal(1)
+      })
+    })
+  })
+})
